fb0caa036e3989dec23a559e70b2ef6637315dbb
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-04-10 21:41:26 +0530

    Add files via upload

---
+++ tut23.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello";
+    cout << "hh";
+   
+    int i=1;
+    while (i<100)
+
+    {
+        cout<<i;
+        i=i+1;
+        /* code */
+    }
+    return 0;
+}
