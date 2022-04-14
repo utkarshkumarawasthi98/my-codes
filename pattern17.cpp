8812c87b0b8874642506389190e45cc4655ee18d
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-04-14 23:13:03 +0530

    Add files via upload

---
+++ pattern17.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        /* code */
+        int j=1;
+        while (j<=i)
+        {
+            /* code */
+            cout<<('A'+j-1)<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+
+
+    return 0;
+}
