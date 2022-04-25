abcb7cbeca5f024c9d592dba339dd3d47522babf
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-04-26 00:39:49 +0530

    Add files via upload

---
+++ pattern24.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        /* code */
+        int j=1;
+        while (j<=n)
+        {
+            /* code */
+            cout<<"z"<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+
+
+    return 0;
+}
