a6492a4f388135d553018f09d0774b278ee03b17
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-06-10 00:13:09 +0530

    Add files via upload

---
+++ code5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int b[n];
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cin>>b[i];
+    }
+    cout<<b[2];
+    
+    return 0;
+}
