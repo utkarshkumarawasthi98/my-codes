9c62c19f0fa9773e6798d6927122137e7c0a98d3
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-06-10 00:15:10 +0530

    Add files via upload

---
+++ code10.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int b[99
+    ] = {1,2,3,4,5,6,7};
+    for (int i = 0; i < 30; i++)
+    {
+        /* code */
+        cout<<b[i]<<" ";
+    }
+    
+    return 0;
+}
