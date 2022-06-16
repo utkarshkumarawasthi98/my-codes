ae436944088d5a7e3e585974230d445a22842f5c
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-06-16 21:12:33 +0530

    Add files via upload

---
+++ code32.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int allindex(int a[], int n,int x,int output[]){
+    if (n==0)
+    {
+        return 0;
+    }
+    int ans=allindex(a+1,n-1,x,output);
+    if (a[0]==x)
+    {
+        for (int i= ans-1 ; i >= 0; i--)
+        {
+            output[i+1]=output[i]+1;
+        }
+        output[0]==0;
+        ans++;
+       
+    }
+    else
+    {
+        for (int i = ans-1; i >= 0; i--)
+        {
+            output[i]=output[i]+1;
+        }
+        
+    }
+    return ans;
+}
+
+
+void pindex(int a[], int n, int x){
+    int output[n];
+    int size=allindex(a,n,x,output);
+    for (int i = 0; i < size; i++)
+    {
+        cout<<output[i]<<" ";
+    }
+}
+
+
+int main(){
+    int a[7]={2,24,5,35,2,3,2};
+    int n=7;
+    int x=2;
+    pindex(a,n,x);
+    return 0;
+}
