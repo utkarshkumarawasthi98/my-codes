620f876899a62f39e5575145d999fdb0fe88546f
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-06-30 01:56:23 +0530

    Add files via upload

--- CODE52.cpp
+++ CODE52.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int allindex(int a[], int n , int x, int output[]){
-    if (n==0)
+int allindex(int a[], int n, int x, int output[])
+{
+    if (n == 0)
     {
         return 0;
     }
-    int ans= allindex(a+1,n-1,x,output);
-    if (a[0]==x)
+    int ans = allindex(a + 1, n - 1, x, output);
+    if (a[0] == x)
     {
-        for (int i = ans-1; i >= 0; i--)
+        for (int i = ans - 1; i >= 0; i--)
         {
-            output[i+1]=output[i]+1;
+            output[i + 1] = output[i] + 1;
         }
-        output[0]=0;
+        output[0] = 0;
         ans++;
-        
     }
     else
     {
-        for (int i = ans-1; i >=0; i--)
+        for (int i = ans - 1; i >= 0; i--)
         {
-            output[i]=output[i]+1;
+            output[i] = output[i] + 1;
         }
-        
     }
     return ans;
 }
-void pindex(int a[], int n, int x){
+void pindex(int a[], int n, int x)
+{
     int output[n];
-    int size=allindex(a,n,x,output);
+    int size = allindex(a, n, x, output);
     for (int i = 0; i < size; i++)
     {
-        cout<<output[i]<<" ";
+        cout << output[i] << " ";
     }
 }
-int main(){
-    int a[7]={2,33,4,45,2,4,2};
-    int n=7;
-    int x=2;
-    pindex(a,n,x);
+int main()
+{
+    int a[7] = {2, 33, 4, 45, 2, 4, 2};
+    int n = 7;
+    int x = 4;
+    pindex(a, n, x);
     return 0;
 }
