2381b9f505e9bcf8463bc2b2eb3431b4d302730c
utkarshkumarawasthi98 <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-07-22 01:27:59 +0530

    Add files via upload

---
+++ code83.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        /* code */
+        int j=1;
+        while (j<=i)
+        {
+            /* code */
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+
+
+    return 0;
+}
