e9364d495a60263e43d99f2cf9559d5b36c1c206
Utkarsh Awasthi <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2022-07-31 01:24:47 +0530

    Add files via upload

---
+++ code101.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cout<<"enter first number";
+    cin>>m;
+    cout<<"enter second number";
+    cin>>n;
+    for (; m<=n; m++)
+    {
+        cout<<m<<endl;
+    }
+    
+    return 0;
+}
