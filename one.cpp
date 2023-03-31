cfd887e433648d52e4576224261d651d5a1a9ef5
Utkarsh Awasthi <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2023-03-31 23:54:21 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    cout<<"enter the number that  ";
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+         cout<<"hello worlddd "<<endl;
+    }
+    
+    return 0;
+}
