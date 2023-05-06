2b220bee73f467c381ee6bd8da5c875fed983ac7
Utkarsh Awasthi <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2023-05-06 21:18:48 +0530

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -1,26 +1,38 @@
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+
+
+
+// int main(){
+//     stack<char> s;
+//     s.push('l');
+//     s.push('o');
+//     s.push('v');
+//     s.push('e');
+//     s.push('u');
+//     cout<<s.size()<<endl;
+
+//     while (!s.empty())
+//     {
+//         /* code */
+//         cout<<"yesss"<<endl;
+//         s.pop();
+//     }
+    
+
+
+//     return 0;
+// }
+
+
 #include<iostream>
-#include<stack>
 using namespace std;
 
 
 
 int main(){
-    stack<char> s;
-    s.push('l');
-    s.push('o');
-    s.push('v');
-    s.push('e');
-    s.push('u');
-    cout<<s.size()<<endl;
-
-    while (!s.empty())
-    {
-        /* code */
-        cout<<"yesss"<<endl;
-        s.pop();
-    }
-    
-
-
+    bool ans=(4>3);
+    cout<<ans<<endl;
     return 0;
 }
