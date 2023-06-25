2470768a370249bb97ecde639ed21b3c3eee85d2
Utkarsh Awasthi <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2023-06-25 23:40:20 +0530

    Add files via upload

--- 2D Hopscotch.cpp
+++ 2D Hopscotch.cpp
@@ -1,62 +1,17 @@
 class Solution{
 public:
-    bool valid(int i, int j, int n, int m) {
-        if(i >= 0 && j >= 0 && i < n && j < m)
-            return true;
-        return false;
-    }
-    
     int hopscotch(int n, int m, vector<vector<int>> mat, int ty, int i, int j)
     {
-        int sum = 0;
-        if(ty == 0) {
-            if(j%2 == 0) {
-                if(valid(i-1, j-1, n, m)) sum += mat[i-1][j-1];
-                if(valid(i-1, j, n, m)) sum += mat[i-1][j];
-                if(valid(i-1, j+1, n, m)) sum += mat[i-1][j+1];
-                if(valid(i, j-1, n, m)) sum += mat[i][j-1];
-                if(valid(i, j+1, n, m)) sum += mat[i][j+1];
-                if(valid(i+1, j, n, m)) sum += mat[i+1][j];
-            }
-            else{
-                if(valid(i-1, j, n, m)) sum += mat[i-1][j];
-                if(valid(i, j-1, n, m)) sum += mat[i][j-1];
-                if(valid(i, j+1, n, m)) sum += mat[i][j+1];
-                if(valid(i+1, j-1, n, m)) sum += mat[i+1][j-1];
-                if(valid(i+1, j, n, m)) sum += mat[i+1][j];
-                if(valid(i+1, j+1, n, m)) sum += mat[i+1][j+1];
+        int ans=0,t=-pow(-1,(j&1));
+        
+        vector<vector<int>> dx={{0,-1,0,1,t,t},{0,-2,0,2,-1,-1,1,1,-t,-t,2*t,2*t}};
+        vector<vector<int>> dy={{-1,0,1,0,-1,1},{-2,0,2,0,-2,2,-2,2,-1,1,-1,1}};
+        for(int k=0;k<6*(ty+1);k++) {
+            int x=i+dx[ty][k],y=j+dy[ty][k];
+            if(x>=0 && x<n && y>=0 && y<m) {
+                ans+=mat[x][y];
             }
         }
-        else{
-            if(j%2 == 0) {
-                if(valid(i-2, j-1, n, m)) sum += mat[i-2][j-1]; 
-                if(valid(i-2, j, n, m)) sum += mat[i-2][j];
-                if(valid(i-2, j+1, n, m)) sum += mat[i-2][j+1]; 
-                if(valid(i-1, j-2, n, m)) sum += mat[i-1][j-2];
-                if(valid(i-1, j+2, n, m)) sum += mat[i-1][j+2]; 
-                if(valid(i, j-2, n, m)) sum += mat[i][j-2];
-                if(valid(i, j+2, n, m)) sum += mat[i][j+2]; 
-                if(valid(i+1, j-2, n, m)) sum += mat[i+1][j-2];
-                if(valid(i+1, j-1, n, m)) sum += mat[i+1][j-1]; 
-                if(valid(i+1, j+1, n, m)) sum += mat[i+1][j+1];
-                if(valid(i+1, j+2, n, m)) sum += mat[i+1][j+2]; 
-                if(valid(i+2, j, n, m)) sum += mat[i+2][j];
-            }
-            else{
-                if(valid(i-1, j-2, n, m)) sum += mat[i-1][j-2]; 
-                if(valid(i-1, j-1, n, m)) sum += mat[i-1][j-1];
-                if(valid(i-2, j, n, m)) sum += mat[i-2][j]; 
-                if(valid(i-1, j+1, n, m)) sum += mat[i-1][j+1];
-                if(valid(i-1, j+2, n, m)) sum += mat[i-1][j+2]; 
-                if(valid(i, j-2, n, m)) sum += mat[i][j-2];
-                if(valid(i, j+2, n, m)) sum += mat[i][j+2]; 
-                if(valid(i+1, j-2, n, m)) sum += mat[i+1][j-2];
-                if(valid(i+1, j+2, n, m)) sum += mat[i+1][j+2]; 
-                if(valid(i+2, j-1, n, m)) sum += mat[i+2][j-1];
-                if(valid(i+2, j, n, m)) sum += mat[i+2][j]; 
-                if(valid(i+2, j+1 ,n, m)) sum += mat[i+2][j+1];
-            }
-        }
-        return sum;
+        return ans;
     }
 };
