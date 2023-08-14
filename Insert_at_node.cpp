83f74689874627912f00fef86051c48ee4d2316a
Utkarsh Awasthi <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2023-08-14 23:08:46 +0530

    Add files via upload

--- Insert_at_node.cpp
+++ Insert_at_node.cpp
@@ -1,45 +1,23 @@
-
 #include<iostream>
 using namespace std;
 
-class node{
-    public:
-    int data;
-    node* next;
 
-    node(int value){
-        data=value;
-        next=NULL;
+ int perm(int i,int j){
+        int answer=1;
+        int n=i;
+        for(int i=1;i<=j;i++){
+            answer=answer*n;
+            n--;
+            answer=answer/(i);
+        }
+        return answer;
     }
-
-
-};
-
-void insertIntoHead(node* &head,int value){
-    node* new_node=new node(value);
-    new_node->next=head;
-    head=new_node;
-
-}
-
-void display(node* head){
-    node*temp=head;
-    while (temp!= NULL)
-    {
-        cout<<temp->data<<"->";
-        temp=temp->next;
-    }
-    
-    cout<<"NULL";
-    
-
-}
 
 
 int main(){
-    node* head=NULL;
-    insertIntoHead(head,22);
-    display(head);
 
-     return 0;
+
+    long long ans=perm(10000,2);
+    cout<<ans;
+    return 0;
 }
