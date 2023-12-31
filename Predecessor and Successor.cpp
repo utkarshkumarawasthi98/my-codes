d07818809394fbcb222e63457aa9abec0b6c4fe7
Utkarsh Awasthi <102986463+utkarshkumarawasthi98@users.noreply.github.com>
2023-12-31 23:50:14 +0530

    Add files via upload

---
+++ Predecessor and Successor.cpp
@@ -0,0 +1,32 @@
+Link to the Problem: https://practice.geeksforgeeks.org/problems/predecessor-and-successor/1
+
+class Solution
+{
+    public:
+    void findPreSuc(Node* root, Node*& pre, Node*& suc, int key){
+    	if (root == NULL) return ;
+    	if (root->key == key){
+    		if (root->left != NULL){
+    			Node* tmp = root->left;
+    			while (tmp->right)
+    				tmp = tmp->right;
+    			pre = tmp ;
+    		}
+    		if (root->right != NULL){
+    			Node* tmp = root->right ;
+    			while (tmp->left)
+    				tmp = tmp->left ;
+    			suc = tmp ;
+    		}
+    		return ;
+    	}
+    	if (root->key > key){
+    		suc = root ;
+    		findPreSuc(root->left, pre, suc, key) ;
+    	}
+    	else {
+    		pre = root ;
+    		findPreSuc(root->right, pre, suc, key) ;
+    	}
+    }
+};
